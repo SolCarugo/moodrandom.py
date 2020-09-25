fe52da12ca448f6007590e46af63ce2fcfaa9611
Maria sol Carugo <69329898+SolCarugo@users.noreply.github.com>
2020-09-24 21:27:16 -0300

    Create moodrandom.py

---
+++ moodrandom.py
@@ -0,0 +1,8 @@
+import random
+
+mood= input("¿estas feliz?")
+
+if mood=="no":  
+    lista=["todo va a mejorar", "no te desanimes", "arriba el animo", "sos genial", "que nadie te venza"]
+    moodValue=random.Random
+    print("hola",lista[moodValue])
